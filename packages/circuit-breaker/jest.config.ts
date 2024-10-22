a8107c73d261c16a84e3cd051a214eefb95a43e8
dyf96 <yingfei.dai96@gmail.com>
2024-10-22 18:34:52 +0900

    feat: ✨ add circuit-breaker pkg

---
+++ packages/circuit-breaker/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'circuit-breaker',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/circuit-breaker',
+};
